9b31850545853df7659c0eefc31a929fa3528428
Andy963 <zjgxs518@126.com>
2021-02-24 22:45:36 +0800

    vue路由相同时，刷新页面解决办法

---
+++ 04_learn_basic/22_Router_refresh.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+export default {
+
+  created() {
+    // 当路由参数变化时，/user/1切换到/user/2原来的组件实例会被利用
+    // 因为两个路由渲染了同一个组件，
+    console.log(this.$route.params.id)
+  },
+  // 解决方法一
+  // watch:{
+  //   $route:(to,from)=>{
+  //     console.log(to.params.id)
+  //     // 发起ajax请示后端接口数据
+  //   }
+  // }
+  // 解决方法二
+  beforeRouteUpdate(to, from, next) {
+    console.log(to.params.id)
+    // 一定要调用next,不会会阻塞整个路由
+    next();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
